7fc0ef1295e96fce1729cf7b1523acc38bc7f7d1
Hirofumi Ueda <hiro.ueda0827@gmail.com>
2016-05-08 20:44:49 +0900

    TODO削除

--- test/src/com/ueda/test/Constants.java
+++ test/src/com/ueda/test/Constants.java
@@ -4,9 +4,9 @@ import java.io.File;
 
 public class Constants {
 
-//	// TODO Jenkins リポジトリのホームをパラメータ化
+//	Jenkins リポジトリのホームをパラメータ化
 //	public static String JENKINS_REPO_HOME = "C:\\Program Files (x86)\\Jenkins\\jobs";
-//	// TODO Jenkins リポジトリ対象をパラメータ化	
+// Jenkins リポジトリ対象をパラメータ化	
 //	final public static String REPO = File.separator + "test_repo";
 	final public static String BUILDS = File.separator + "builds";
 	final public static String OPEN_TASKS_XML_FILE = "open-tasks.xml";
